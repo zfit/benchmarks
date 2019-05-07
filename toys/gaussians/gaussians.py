e4d468374a37a78626167c7cdc28fb55ff36ab41
Jonas Eschle 'Mayou36 <mayou36@jonas.eschle.com>
2019-05-08 00:50:02 +0200

    Fix childs

--- toys/gaussians/gaussians.py
+++ toys/gaussians/gaussians.py
@@ -120,8 +120,8 @@ if __name__ == '__main__':
 
     # run_zfit = False
     run_zfit = True
-    n_gauss_max = 25
-    n_params_max = 25
+    n_gauss_max = 35
+    n_params_max = n_gauss_max
     toys_nevents = [2 ** i for i in range(7, 23)]
     n_toys = 100
     results = {}
