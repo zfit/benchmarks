1ee8eed850ae3b2a71c782f2e6faa62833c1cc46
Jonas Eschle 'Mayou36 <mayou36@jonas.eschle.com>
2019-05-09 17:30:46 +0200

    Remove HACK

--- toys/gaussians/gaussians.py
+++ toys/gaussians/gaussians.py
@@ -13,7 +13,7 @@ zfit.run.numeric_checks = False
 
 
 def toy_run(n_params, n_gauss, n_toys, toys_nevents, run_zfit, intermediate_result_factory=None):
-    # zfit.run.create_session(reset_graph=True)
+    zfit.run.create_session(reset_graph=True)
 
     # pdf = chebys[0]
 
