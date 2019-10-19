beafad8f59e1509447e5d8e04ba76ee04f7bd259
Jonas Eschle 'Mayou36 <mayou36@jonas.eschle.com>
2019-10-20 00:40:13 +0100

    Add some more playing around

--- code/py_function.py
+++ code/py_function.py
@@ -4,8 +4,8 @@ import torch
 import numpy as np
 from zfit_benchmark.timer import Timer
 
-tf.enable_v2_behavior()
-
+# tf.enable_v2_behavior()
+import zfit
 
 def calc_np(x):
     x = x.numpy()
@@ -27,17 +27,22 @@ def calc_tf(x):
 
 
 def calc_torch(x):
+    x = torch.tensor(x.numpy())
     x = torch.sqrt(torch.abs(x))
     x = torch.cos(x - 0.3)
     x = torch.sinh(x + 0.4)
     x = x ** 2
     x = torch.sum(torch.log(x))
-    return x
+    return x.numpy()
 
 
 @tf.function
 def calc_np_wrapped(x):
     return tf.py_function(calc_np, [x], Tout=tf.float32)
+
+@tf.function
+def calc_torch_wrapped(x):
+    return tf.py_function(calc_torch, [x], Tout=tf.float32)
 
 
 @numba.jit(nopython=True)
@@ -53,14 +58,26 @@ def calc_np_numba(x):
 if __name__ == '__main__':
     size = (1000000,)
     x = tf.random.normal(shape=size)
-    # x = torch.normal(0, 1, size=size)
     # x = x.numpy()
+    # y = zfit.run(calc_tf(x))
+    # x = zfit.run(x)
+    results = []
+    calc_tf_graph = calc_tf(x)
+    calc_np_wrapped_graph = calc_np_wrapped(x)
+    # grad = tf.gradients(calc_np_wrapped_graph, x)
+    grad = tf.gradients(calc_tf_graph, x)
+    print(zfit.run(grad))
     with Timer() as timer:
         for _ in range(100):
             # y = calc_np_wrapped(x)
             # y = calc_np(x)
-            y = calc_tf(x)
+            # y = calc_tf(x)
+            # y = calc_torch_wrapped(x)
+            # y = zfit.run(calc_tf_graph)
+            y = zfit.run(calc_np_wrapped_graph)
+            # x = torch.normal(0, 1, size=size)
             # y = calc_torch(x)
             # y = calc_np_numba(x)
-    print(y)
+            results.append(y)
+    print(f"{np.average(results)} +- {np.std(results)}")
     print(timer.elapsed * 1000, 'ms')
