d70f961d0b8fb85b76b4eb7b6df83c8d86ff045a
Jonas Eschle 'Mayou36 <mayou36@jonas.eschle.com>
2020-03-08 22:25:01 +0100

    Add broadcast integration example

--- code/params_vs_argument.py
+++ code/params_vs_argument.py
@@ -1,46 +1,62 @@
+import progressbar
 import tensorflow as tf
 from zfit_benchmark.timer import Timer
 
 var1 = tf.Variable(42.)
 
+size_int_sample = 20000
 
+
+@tf.function(autograph=False)
 def func(x, y):
     return (x - 1 / y) ** 2 - y ** tf.abs(1 + x)
 
 
 def func_params(x, y):
     var1.assign(y)
-    return func(x=x, y=var1)
+    vals = func(x=x, y=var1)
+    tf.debugging.assert_equal(var1.value(), y)
+    return vals
 
 
 def func_args(x, y):
-    return func(x, y)
+    vals = func(x, y)
+    tf.debugging.assert_equal(y + 1, y + 1)
+    return vals
 
 
 def integrate_func_params(y):
-    x = tf.random.uniform(shape=(20000,), minval=-1., maxval=1.)
+    x = tf.random.uniform(shape=(size_int_sample,), minval=-1., maxval=1.)
     return tf.reduce_mean(func_params(x=x, y=y))
 
 
 def integrate_func_args(y):
-    x = tf.random.uniform(shape=(20000,), minval=-1., maxval=1.)
-    return tf.reduce_mean(func_args(x=x, y=y))
+    x = tf.random.uniform(shape=(size_int_sample,), minval=-1., maxval=1.)
+    return tf.reduce_mean(func_args(x=x, y=y), axis=-1)
+
 
 @tf.function(autograph=False)
 def integrate(y, func):
-    return tf.map_fn(func, x)
+    return tf.map_fn(func, y, parallel_iterations=14)
+
+@tf.function(autograph=False)
+def integrate_broadcast(y, func):
+    y = y[:, None]
+    return func(y)
     # return tf.vectorized_map(func, x)
 
 
 if __name__ == '__main__':
-    size = (10000,)
+    size = (50000,)
     x = tf.random.normal(shape=size)
     results = []
-    n_trials = 10
+    n_trials = 3
     with Timer() as timer:
-        for _ in range(n_trials):
+        for _ in progressbar.progressbar(range(n_trials)):
             x = tf.random.normal(shape=size)
-            # integrate(x, integrate_func_args)
-            integrate(x, integrate_func_params)
+            result = integrate(x, integrate_func_args)
+            # result = integrate_broadcast(x, integrate_func_args)
+            # result = integrate(x, integrate_func_params)
 
+    print(f"Result = {result}")
     print(f"Time needed (per run): {timer.elapsed / n_trials :.3} sec")
--- code/py_function.py
+++ code/py_function.py
@@ -1,22 +1,37 @@
+"""Benchmark of different frameworks for playing around.
+
+no GPU used, serial required (x used in ever next calculation)
+| sample_size | tf traced  | tf eager | numpy |
+| 1k        | 0.001      | 0.004      | 0.001 |
+| 10 k      | 0.005      | 0.013      | 0.007 |
+| 100 k     | 0.015      | 0.03       | 0.07  |
+| 1 mio     | 0.2        | 0.3        | 0.7   |
+| 10 mio    | 2          | 3          | 7     |
+
+
+no GPU used, parallel possible, list of 10
+| sample_size | tf traced  | tf eager | numpy | torch |
+| 1k        | 0.0002     | 0.004      | 0.001 | 0.001 |
+| 10 k      | 0.0008     | 0.014      | 0.008 | 0.004 |
+| 100 k     | 0.002      | 0.04       | 0.08  | 0.02  |
+| 1 mio     | 0.02       | 0.4        | 0.8   | 0.3   |
+| 10 mio    | 0.2        | 4          | 8     | 3     |
+            (with autograph 2 secs)
+
+"""
+
 import numba as numba
+import numpy as np
 import tensorflow as tf
 import torch
-import numpy as np
 from zfit_benchmark.timer import Timer
-
-v2behavior = True
-# v2behavior = False
-if v2behavior:
-    tf.enable_v2_behavior()
-import zfit
-
-if not v2behavior:
-    tf.disable_eager_execution()
 
 global_y = tf.random.normal(shape=(10, 1))
 var1 = tf.Variable(42.)
 
 list1 = [1, 2, 3, 4]
+size = (10000000,)
+n_loops = 10
 
 
 def dummy():
@@ -25,38 +40,67 @@ def dummy():
 
 
 def calc_np(x):
-    x = x.numpy()
-    dummy()
+    # x = x.numpy()
+    # dummy()
     # x = x * global_y.numpy()
-    x = zfit.run(x * global_y)
+    # x = zfit.run(x * global_y)
     # x *= var1.numpy()
-    x = np.sqrt(np.abs(x))
-    x = np.cos(x - 0.3)
-    x = np.sinh(x + 0.4)
-    x = x ** 2
-    x = np.sum(np.log(x))
+    x_init = x
+    list1 = []
+    for i in range(n_loops):
+        x = np.sqrt(np.abs(x_init))
+        x = np.cos(x - 0.3)
+        x = np.power(x, i + 1)
+        x = np.sinh(x + 0.4)
+        x = x ** 2
+        x += np.random.normal(size=size)
+        x /= np.mean(x)
+        x = np.abs(x)
+        list1.append(x)
+    x = np.sum(list1, axis=0)
+    x = np.mean(np.log(x))
     return x
 
 
-@tf.function
+@tf.function(autograph=False)
 def calc_tf(x):
-    x = tf.sqrt(tf.abs(x))
-    x = tf.cos(x - 0.3)
-    x = tf.sinh(x + 0.4)
-    print("calc_tf is being traced")
-    x = x ** 2
-    x = tf.reduce_sum(tf.log(x))
-    tf.py_function(dummy, [], Tout=[])
+    x_init = x
+    list1 = []
+    for i in tf.range(n_loops):
+    # for i in range(n_loops):
+        x = tf.sqrt(tf.abs(x_init))
+        x = tf.cos(x - 0.3)
+        x = tf.pow(x, tf.cast(i + 1, tf.float64))
+        x = tf.sinh(x + 0.4)
+        # print("calc_tf is being traced")
+        x = x ** 2
+        x += tf.random.normal(shape=size, dtype=tf.float64)
+        x /= tf.reduce_mean(x)
+        x = tf.abs(x)
+        list1.append(x)
+    x = tf.reduce_sum(x, axis=0)
+    x = tf.reduce_mean(tf.math.log(x))
+    # tf.py_function(dummy, [], Tout=[])
     return x
 
 
+# @torch.jit.script
 def calc_torch(x):
-    x = torch.tensor(x.numpy())
-    x = torch.sqrt(torch.abs(x))
-    x = torch.cos(x - 0.3)
-    x = torch.sinh(x + 0.4)
-    x = x ** 2
-    x = torch.sum(torch.log(x))
+    x_init = x
+    list1 = []
+    for i in range(n_loops):
+        x = torch.sqrt(torch.abs(x_init))
+        x = torch.cos(x - 0.3)
+        x = torch.pow(x, i + 1)
+        x = torch.sinh(x + 0.4)
+        x = x ** 2
+        x += torch.normal(mean=0, std=0, size=size)
+        x /= torch.mean(x)
+        x = torch.abs(x)
+        list1.append(x)
+    list1 = torch.stack(list1)
+    x = torch.sum(list1, dim=0)
+    x = torch.mean(torch.log(x))
     return x.numpy()
 
 
@@ -72,17 +116,20 @@ def calc_torch_wrapped(x):
 
 @numba.jit(nopython=True)
 def calc_np_numba(x):
-    x = np.sqrt(np.abs(x))
-    x = np.cos(x - 0.3)
-    x = np.sinh(x + 0.4)
-    x = x ** 2
-    x = np.sum(np.log(x))
+    for i in range(n_loops):
+        x = np.sqrt(np.abs(x))
+        x = np.cos(x - 0.3)
+        x = np.power(x, i)
+        x = np.sinh(x + 0.4)
+        x = x ** 2
+        x = np.mean(np.log(x))
+        x += np.random.normal(size=size)
     return x
 
 
 if __name__ == '__main__':
-    size = (100000,)
-    x = tf.random.normal(shape=size)
+    x_tf = tf.random.normal(shape=size, dtype=tf.float64)
+    x_torch = torch.normal(mean=0, std=0, size=size)
     # x = x.numpy()
     # y = zfit.run(calc_tf(x))
     # x = zfit.run(x)
@@ -92,24 +139,32 @@ if __name__ == '__main__':
     # grad = tf.gradients(calc_np_wrapped_graph, x)
     # grad = tf.gradients(calc_tf_graph, x)
     # print(zfit.run(grad))
+    # x = np.random.normal(size=size)
+    y = calc_np(x_tf)
+    y = calc_tf(x_tf)
+    y = calc_torch(x_torch)
+
+    # y = calc_np_numba(x)
+
     with Timer() as timer:
-        for _ in range(100):
-            x = tf.random.normal(shape=size)
-            with tf.GradientTape() as tape:
-                tape.watch(x)
-                y = calc_np_wrapped(x)
-                # y = calc_np(x)
-                # y = calc_tf(x)
-                # y = calc_torch_wrapped(x)
-                # y = zfit.run(calc_tf_graph)
-                # y = zfit.run(calc_np_wrapped_graph)
-                # x = torch.normal(0, 1, size=size)
-                # y = calc_torch(x)
-                # y = calc_np_numba(x)
-                # if not v2behavior:
-                #     zfit.run()
-            gradients = tape.gradient(y, x)
-            print(gradients)
+        n_runs = 5
+        for _ in range(n_runs):
+            # x = tf.random.normal(shape=size)
+            # with tf.GradientTape() as tape:
+            #     tape.watch(x)
+            # y = calc_np_wrapped(x)
+            # y = calc_np(x_tf)
+            # y = calc_tf(x_tf)
+            y = calc_torch(x_torch)
+            # y = calc_torch_wrapped(x)
+            # y = zfit.run(calc_tf_graph)
+            # y = zfit.run(calc_np_wrapped_graph)
+            # x = torch.normal(0, 1, size=size)
+            # y = calc_np_numba(x)
+            # if not v2behavior:
+            #     zfit.run()
+            # gradients = tape.gradient(y, x)
+            # print(gradients)
             results.append(y)
     print(f"{np.average(results)} +- {np.std(results)}")
-    print(f"Time needed: {timer.elapsed :.3} sec")
+    print(f"Time needed: {timer.elapsed / n_runs :.3} sec")
